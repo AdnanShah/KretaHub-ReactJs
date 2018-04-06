9b7d89e7bc11558283f4eb47726586df52838e98
Mirza Umer Saleem <mirzaumersaleem@yahoo.com>
2018-04-06 19:01:55 +0500

    /ThankYou added

--- src/containers/EmailTemplate.js
+++ src/containers/EmailTemplate.js
@@ -8,26 +8,67 @@ const divStyle={
     float: 'center', height:'100%',
     position:'relative', backgroundColor:"#494c50",
     valign:"top", backgroundSize: 'cover', backgroundPosition: 'center',
-    backgroundImage: `url(${Background})`
+    backgroundImage: `url(${Background})`,
+    position:'relative',
 };
 const styles = theme => ({
     root: theme.mixins.gutters({
       paddingTop: 16,
       paddingBottom: 16,
       marginTop: theme.spacing.unit * 3,
+      width:'45%',
+      top: '50%',
+      left: '50%',
+      marginRight: '-50%',
+      transform: `translate(-50%, -50%)`,
+      position:'absolute',
     }),
-  });
+  }); 
+const label= {
+  /* Other styling.. */
+  textAlign: 'right',
+  clear: 'both',
+  float:'left',
+  marginRight:'15px',
+} 
   function PaperSheet(props) {
     const { classes } = props;
     return (
-      <div>
-        <Paper className={classes.root} elevation={4}>
-          <Typography variant="headline" component="h3">
-            This is a sheet of paper.
+      <div style={divStyle}>
+        <h1 style={{color:'white'}}>Your Shipper Signup Request is Being Processed</h1>
+        <Paper className={classes.root} elevation={4} >
+          <Typography variant="headline" component="p">
+          Thank you for signing up as a shipper on KretaHub.		
           </Typography>
-          <Typography component="p">
-            Paper can be used to build surface or other elements for your application.
+          <br/>
+          <Typography  component="p" style={{fontWeight:'bold'}}>
+          Shipper Information			
           </Typography>
+          <div class="form-group">
+            <label for="Student">Name:  </label>
+            <label style={{marginLeft:'20px'}} name="Student">Umer Saleem</label>
+            <br/>
+            <label for="Student">Email:  </label>
+            <label style={{marginLeft:'20px'}} name="Student">UmerSaleem@gmail.com</label>
+          </div>
+          {/* <div class="form-group">
+            <label for="Student">Email:  </label>
+            <label style={{marginLeft:'20px'}} name="Student">UmerSaleem@gmail.com</label>
+          </div> */}
+   
+          <Typography  component="label" >
+          We will notify you by email when your request has been approved.				
+          </Typography>
+          <br/>
+          <Typography  component="p" >
+          In the mean time, you can follow us on:						
+          </Typography>
+          <br/>
+          <footer style={{backgroundColor:'#F3F3F3',alignContent:'center'}}>
+            <a style={{marginLeft:'5px',color:'#20B0A8'}} href='www.raqamisolutions.com'>Facebook</a>
+            <a style={{marginLeft:'20px',color:'#20B0A8'}}href='www.raqamisolutions.com'>Instagram</a>
+            <a style={{marginLeft:'20px',color:'#20B0A8'}} href='www.raqamisolutions.com'>Twitter</a>
+          </footer>
         </Paper>
       </div>
     );
