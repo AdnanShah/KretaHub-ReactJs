c28ee7f3c91520f1cc3cc7a2083cb60c68f11987
AdnanShah <adnanshah9911@gmail.com>
2018-05-11 02:32:32 -0700

    template update

---
+++ Email Template/js/pendings.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+function loadUser() {
+    var template = $("#template").html();
+    Mustache.parse(template); // optional, speeds up future uses
+    var rendered = Mustache.render(template,
+      {
+          "main": {
+            "heading": " Your Shipper Signup Request is Being Processed",
+            "userName": "PT. Indotech",
+            "subHeading": "Thank you for signing up as a shipper on KretaHub.",
+            "subHeading2": "We will send you a follow-up email when your request has been approved.",
+            "followUs": "In the mean time, you can follow us on:",
+            "companyName": "KretaHub"
+        }});
+    $("#target").html(rendered);
+  }
+  
