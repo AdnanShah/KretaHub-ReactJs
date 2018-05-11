8b6c6365f34de81c8232bdccf36ed8455c97a7e4
AdnanShah <adnanshah9911@gmail.com>
2018-05-11 05:59:29 -0700

    web updated

--- Email Template/js/ThankYou.js
+++ Email Template/js/ThankYou.js
@@ -6,5 +6,4 @@
   document.getElementById("userName").innerHTML = mydata[0].userName;
   document.getElementById("userEmail").innerHTML = mydata[0].userEmail;
   document.getElementById("conformNote").innerHTML = mydata[0].conformNote;
-  document.getElementById("followUs").innerHTML = mydata[0].followUs;
 })();
